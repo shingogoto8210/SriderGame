94bee1ac72117dd075774cc7b43a003590bbc4e1
shingogoto8210 <chupps8210@gmail.com>
2021-05-29 15:12:09 +0900

    手順１～６　完了

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject playerObj;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - playerObj.transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerObj != null)
+        {
+            transform.position = playerObj.transform.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("移動速度")]
+    public float moveSpeed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //移動
+        Move();
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3(x * moveSpeed, rb.velocity.y, rb.velocity.z);
+
+        Debug.Log(rb.velocity);
+    }
+}
