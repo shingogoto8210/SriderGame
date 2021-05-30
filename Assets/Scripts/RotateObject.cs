507d94532f74999da40d06ee859c05bd5ce3fd3a
shingogoto8210 <chupps8210@gmail.com>
2021-05-30 20:53:13 +0900

    手順１４　完了

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RotateObject : MonoBehaviour
+{
+    [Header("回転秒数")]
+    public float duaring;
+
+    private bool isRotate;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.tag == "Player" && isRotate ==false)
+        {
+            Rotate();
+        }
+    }
+
+    private void Rotate()
+    {
+        Vector3 Rotation = new Vector3(0, 0, RotateAngle());
+        transform.DORotate(Rotation, duaring);
+        isRotate = true;
+    }
+
+    private float RotateAngle()
+    {
+        int value = Random.Range(0, 2);
+        //return value == 0 ? 70 : -70; 
+        if(value == 0)
+        {
+            return 70f;
+        }
+        else
+        {
+            return -70f;
+        }
+    }
+}
