dfbe91117a900fbc266273d799bede31c225c7d8
shingogoto8210 <chupps8210@gmail.com>
2021-06-06 20:30:31 +0900

    手順２１完了

---
+++ Assets/Scripts/FrostEffectController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrostEffectController : MonoBehaviour
+{
+    private FrostEffect frostEffect;
+    void Start()
+    {
+        frostEffect = GetComponent<FrostEffect>();
+        frostEffect.FrostAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(frostEffect.FrostAmount >= 0)
+        {
+            frostEffect.FrostAmount -= Time.deltaTime/20;
+        }
+        if (frostEffect.FrostAmount <= 0)
+        {
+            frostEffect.FrostAmount = 0;
+        }
+    }
+
+    public void AddFrostAmount(float amount)
+    {
+        frostEffect.FrostAmount += amount;
+    }
+}
---
+++ Assets/Scripts/FrostEffector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrostEffector : MonoBehaviour
+{
+    private float duration = 0.3f;
+    private bool isApplyEffected;
+    
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player" && isApplyEffected == false)
+        {
+            isApplyEffected = true;
+            Camera.main.gameObject.GetComponent<FrostEffectController>().AddFrostAmount(duration);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
